c976242d23538f155814c5c362155f5488812414
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-04-05 10:19:34 +0530

    myblock

---
+++ myblock/lang/en/block_myblock.php
@@ -0,0 +1,7 @@
+<?php
+
+    $string['pluginname']='Actions of courses';
+    $string['myblock']='My Block';
+    $string['overview']='overview of students';
+    $string['myblock:addinstance']='Add a new My Block block';
+    $string['myblock:myaddinstance']='Add a new My Block block to the my moodle page';
