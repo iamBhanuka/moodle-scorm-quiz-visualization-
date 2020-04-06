f947a2488dc909edd1c65ff3b98f9d2819005dca
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-04-06 10:33:27 +0530

    change dropdown

--- myblock/overview.php
+++ myblock/overview.php
@@ -48,30 +48,40 @@
     echo $OUTPUT->heading($title, 2);
 
     echo $OUTPUT->container_start('block_myblock');
-    echo html_writer::start_tag('form', array('action' =>'overview.php', 'method' => 'post'));
-    echo html_writer::select( $acdemicyear,'per1',$selected1,true).' ';
-    echo html_writer::select( $types,'per2',$selected2,true).' ';
-    echo html_writer::select( $years,'per3',$selected3,true).' ';
-    echo html_writer::select( $semesters,'per4',$selected4,true).' ';
-    echo html_writer::select( $ndayss,'per5',$selected5,true).' ';
-    echo html_writer::select( $actions,'per6',$selected6,true).' ';            
-    echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'logingraphid', 'value' => $id));
-    echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'courseid', 'value' => $courseid));
-    echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'userid', 'value' => $userid));
 
-    echo html_writer::empty_tag('input', array('type' => 'submit', 'class' => 'btn-primary', 'value' => 'courses summary','style'=>'height:35px ; border:1px solid black'));
-echo html_writer::end_tag('form').'<br>';       
-echo html_writer::end_tag('div');
+        $acdemicyear=array('Academic year 2020','Academic year 2019','Academic year 2018');
+        $types=array('SCS','IS');
+        $years=array('1 st Year','2 nd Year','3 rd Year','4 th Year');
+        $semesters=array('1 st Semester','2 nd Semester');
+        $ndayss=array('30','60','90','105');
+        $actions=array('viewed','All Actions');
 
-echo html_writer::start_tag('div', array('style' => 'border-style:groove ; '));     
-$id2=$acdemicyear[ $_POST['per1'] ];
-$type=$types[ $_POST['per2'] ];
-$uyear=$years[ $_POST['per3'] ];
-$semester=$semesters[ $_POST['per4'] ];
-$ndays=$ndayss[ $_POST['per5'] ];
-$action=$actions[ $_POST['per6'] ];         
-get_course_data($id2,$type,$uyear,$semester,$ndays,$action) ;
-echo html_writer::end_tag('div');
+        echo html_writer::start_tag('div');
+            echo html_writer::start_tag('form', array('action' =>'overview.php', 'method' => 'post'));
+                echo html_writer::select( $acdemicyear,'per1',$selected1,true).' ';
+                echo html_writer::select( $types,'per2',$selected2,true).' ';
+                echo html_writer::select( $years,'per3',$selected3,true).' ';
+                echo html_writer::select( $semesters,'per4',$selected4,true).' ';
+                echo html_writer::select( $ndayss,'per5',$selected5,true).' ';
+                echo html_writer::select( $actions,'per6',$selected6,true).' ';            
+                echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'logingraphid', 'value' => $id));
+                echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'courseid', 'value' => $courseid));
+                echo html_writer::empty_tag('input', array('type' => 'hidden', 'name' => 'userid', 'value' => $userid));
+
+                echo html_writer::empty_tag('input', array('type' => 'submit', 'class' => 'btn-primary', 'value' => 'courses summary','style'=>'height:35px ; border:1px solid black'));
+            echo html_writer::end_tag('form').'<br>';       
+        echo html_writer::end_tag('div');
+
+        echo html_writer::start_tag('div', array('style' => 'border-style:groove ; '));     
+            $id2=$acdemicyear[ $_POST['per1'] ];
+            $type=$types[ $_POST['per2'] ];
+            $uyear=$years[ $_POST['per3'] ];
+            $semester=$semesters[ $_POST['per4'] ];
+            $ndays=$ndayss[ $_POST['per5'] ];
+            $action=$actions[ $_POST['per6'] ];         
+            get_course_data($id2,$type,$uyear,$semester,$ndays,$action) ;
+        echo html_writer::end_tag('div');
+        
     echo $OUTPUT->container_end();
 
     echo $OUTPUT->footer();
