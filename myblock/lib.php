1652b2717de9fcb1c997dc9fd0334553dc3d6e2f
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-04-07 10:36:11 +0530

    chnaged lib

--- myblock/lib.php
+++ myblock/lib.php
@@ -11,7 +11,49 @@
                 }
         }
 
+        // selcect category id
+        function get_course_data( $id,$type,$uyear,$semester,$ndays,$action){            
+                global $DB,$sid,$sc;
+
+                $sc=0;   
+                $subject=array();                   
+                
+                $sql=  "SELECT id FROM {course_categories} WHERE parent
+                        IN(SELECT id FROM {course_categories} WHERE parent 
+                           IN(SELECT id FROM {course_categories} WHERE  parent 
+                              IN(SELECT id FROM {course_categories}  WHERE name='$id')AND name='$type')AND name='$uyear')
+                        AND name='$semester';";
+
+                $categorys=$DB->get_records_sql($sql);
+                if(count($categorys)>0 && $ndays>0){
+                        
+                        echo ' '.$id.'  '; echo $type.'  ';echo $uyear.'  '; echo $semester.' course '.$action.' summary in '.$ndays.' days'.'<br>'.'<br>';
+                        foreach($categorys as $top=>$value){
+                                $sid=$value->id;
+                        }
+                
+                        $sql1="SELECT id FROM {course} WHERE category='$sid';";
+                        $course=$DB->get_records_sql($sql1);
+                        foreach($course as $sub=>$value){
+                                $subject[$sc]=$value->id;
+                                $sc++;
+                        }                        
+                        get_login_data($subject,$ndays,$action); 
+                }
+               
+                else{   if($ndays=='Select no of days'){
+                              echo 'please select number of days which you want';
+                       
+                        } 
+                        else{
+                                echo  ' In  '.$id. ' there is no values to display'.'<br>'.'<br>';
+                        }
+                         
+                }                  
+        };
         
+        
+
         
 
        
