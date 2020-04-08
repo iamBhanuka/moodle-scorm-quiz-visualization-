ceacfb6497424cf3ac7d271470ce33d25f2b943d
unknown <SanduniAbenayaka>
2020-04-08 08:52:34 +0530

    create class

--- studentaction/block_studentaction.php
+++ studentaction/block_studentaction.php
@@ -3,54 +3,54 @@
 require_once(dirname(__FILE__) . '/../../config.php');
 
 
-class block_myblock extends block_base {
+ class block_myblock extends block_base {
 
-    public function init(){
-        $this->title='course login data';
-    }
+    // public function init(){
+    //     $this->title='course login data';
+    // }
 
-    public function get_content(){
-        global $USER, $COURSE, $CFG, $OUTPUT, $DB;
+//     public function get_content(){
+//         global $USER, $COURSE, $CFG, $OUTPUT, $DB;
 
-         if($this->content!==null){
-             return $this->content;
-         }
+//          if($this->content!==null){
+//              return $this->content;
+//          }
 
-         $this->content=new stdClass;
-         $this->content->text='';
-         $this->content->footer='';
+//          $this->content=new stdClass;
+//          $this->content->text='';
+//          $this->content->footer='';
          
        
-         $parameters=array( 'logingraphid' => $this->instance->id,'courseid' => $COURSE->id,'userid'=>$USER->id);
-         $options = array('class' => 'overviewButton');
-         $url = new moodle_url('/blocks/myblock/overview.php',$parameters);
-         $this->content->text .= $OUTPUT->single_button($url, 'overview of students', 'post',$options);
+//          $parameters=array( 'logingraphid' => $this->instance->id,'courseid' => $COURSE->id,'userid'=>$USER->id);
+//          $options = array('class' => 'overviewButton');
+//          $url = new moodle_url('/blocks/myblock/overview.php',$parameters);
+//          $this->content->text .= $OUTPUT->single_button($url, 'overview of students', 'post',$options);
          
-         return $this->content;
-    }
+//          return $this->content;
+//     }
 
-    public function specialization(){
-         if(isset($this->config)){
+//     public function specialization(){
+//          if(isset($this->config)){
             
-         }
-    }
+//          }
+//     }
 
-    public function instance_allow_multiple(){
-         return true;
-    }
+//     public function instance_allow_multiple(){
+//          return true;
+//     }
 
-    function has_config(){
-          return true;
-    }
+//     function has_config(){
+//           return true;
+//     }
 
-    public function applicable_formats(){
-        return array(
-            'course-view'    => true,  
-            'site'           => true,
-            'mod'            => false,
-            'my'             => true        
-        );
-    }
+//     public function applicable_formats(){
+//         return array(
+//             'course-view'    => true,  
+//             'site'           => true,
+//             'mod'            => false,
+//             'my'             => true        
+//         );
+//     }
       
-}
+// }
  
