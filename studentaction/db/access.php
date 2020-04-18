95fec59199501af422ed8b00170bafaf53430282
unknown <SanduniAbenayaka>
2020-04-18 09:03:48 +0530

    add addinstance

--- studentaction/db/access.php
+++ studentaction/db/access.php
@@ -11,17 +11,17 @@
             'clonepermissionsfrom'=>'moodle/my:manageblocks'
         ),
 
-        // 'block/myblock:addinstance'=>array(
-        //     'riskbitmask'=> RISK_SPAM | RISK_XSS,
+        'block/myblock:addinstance'=>array(
+            'riskbitmask'=> RISK_SPAM | RISK_XSS,
 
-        //     'captype'=>'write',
-        //     'contextlevel'=>CONTEXT_BLOCK,
-        //     'archetypes'=>array(
-        //         'editingteacher'=>CAP_ALLOW,
-        //         'manager'=>CAP_ALLOW
-        //     ),
+            'captype'=>'write',
+            'contextlevel'=>CONTEXT_BLOCK,
+            'archetypes'=>array(
+                'editingteacher'=>CAP_ALLOW,
+                'manager'=>CAP_ALLOW
+            ),
 
-        //     'clonepermissionsfrom'=>'moodle/site:manageblocks'
-        // ),
+            'clonepermissionsfrom'=>'moodle/site:manageblocks'
+        ),
 
     );
