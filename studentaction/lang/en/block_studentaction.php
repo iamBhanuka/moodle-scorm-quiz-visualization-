8efd509da89576db99ab432d37e6f1ea06694146
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-04-19 09:05:39 +0530

    rename

---
+++ studentaction/lang/en/block_studentaction.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname']='My Block block';
+$stinrg['myblock']='My Block';
+$stinrg['overview']='overview of students';
+$stinrg['myblock:addinstance']='Add a new My Block block';
+$stinrg['myblock:myaddinstance']='Add a new My Block block to the my moodle page';
