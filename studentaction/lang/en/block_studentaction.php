8784186c78fae00519733dfa25f42d788ba6b15c
unknown <SanduniAbenayaka>
2020-04-20 09:06:27 +0530

    change plugin name

--- studentaction/lang/en/block_studentaction.php
+++ studentaction/lang/en/block_studentaction.php
@@ -1,5 +1,5 @@
 <?php
-$string['pluginname']='My Block block';
+$string['pluginname']='Student action ';
 $stinrg['myblock']='My Block';
 $stinrg['overview']='overview of students';
 $stinrg['myblock:addinstance']='Add a new My Block block';
