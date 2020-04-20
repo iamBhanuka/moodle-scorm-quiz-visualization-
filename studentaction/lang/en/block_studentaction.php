ba1104503783f4d4d96c4a39059583c5c644480f
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-04-20 09:08:03 +0530

    change overview

--- studentaction/lang/en/block_studentaction.php
+++ studentaction/lang/en/block_studentaction.php
@@ -1,6 +1,6 @@
 <?php
 $string['pluginname']='Student action ';
 $stinrg['studentaction']='Student action';
-$stinrg['overview']='overview of students login';
+$stinrg['overview']='overview of students action details';
 $stinrg['myblock:addinstance']='Add a new My Block block';
 $stinrg['myblock:myaddinstance']='Add a new My Block block to the my moodle page';
