8ad87d10efe53cb04fa35272d6240c2aa5214563
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-04-20 09:07:31 +0530

    change plugin

--- studentaction/lang/en/block_studentaction.php
+++ studentaction/lang/en/block_studentaction.php
@@ -1,6 +1,6 @@
 <?php
 $string['pluginname']='Student action ';
-$stinrg['myblock']='My Block';
-$stinrg['overview']='overview of students';
+$stinrg['studentaction']='Student action';
+$stinrg['overview']='overview of students login';
 $stinrg['myblock:addinstance']='Add a new My Block block';
 $stinrg['myblock:myaddinstance']='Add a new My Block block to the my moodle page';
