416f54ca07a5a6cf5dce314dfce0a88d6b9cb17a
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-04-22 09:11:10 +0530

    delete

--- studentaction/lang/en/block_myblock.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string['pluginname']='My Block block';
-$stinrg['myblock']='My Block';
-$stinrg['overview']='overview of students';
-$stinrg['myblock:addinstance']='Add a new My Block block';
-$stinrg['myblock:myaddinstance']='Add a new My Block block to the my moodle page';
