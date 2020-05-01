9bb0a683d9a2e8eaec50d38c30868e685e5f4158
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-05-02 02:46:46 +0530

    commit

--- tracker2/settings.php
+++ tracker2/settings.php
@@ -5,3 +5,9 @@ $settings->add(new admin_setting_heading(
             get_string('descconfig', 'block_tracker2')
         ));
  
+        $settings->add(new admin_setting_configcheckbox(
+            'tracker2/Allow_HTML',
+            get_string('labelallowhtml', 'block_tracker2'),
+            get_string('descallowhtml', 'block_tracker2'),
+            '0'
+        ));
