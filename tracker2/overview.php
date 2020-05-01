e0680ff20cb8586977b6575160f28acd4b3311ce
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-05-02 02:50:39 +0530

    initiate

--- tracker2/overview.php
+++ tracker2/overview.php
@@ -8,7 +8,19 @@
    define('DEFAULT_PAGE_SIZE', 20);
    define('SHOW_ALL_PAGE_SIZE', 5000);
 
+   $id              = required_param('tracker2id',    PARAM_INT);
+   $courseid        = required_param('courseid',   PARAM_INT);
+   $userid          = required_param('userid',     PARAM_INT);
 
+   $page            = optional_param('page', 0,    PARAM_INT); 
+   $perpage         = optional_param('perpage',    DEFAULT_PAGE_SIZE, PARAM_INT); 
+   $group           = optional_param('group', 0,   PARAM_INT); 
+
+   $course          = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+   $context         = block_tracker2_course_context($courseid);
+
+   $loginblock      = $DB->get_record('block_instances', array('id' => $id), '*', MUST_EXIST);
+   $loginsconfig    = unserialize(base64_decode($loginblock->configdata));
 
    $PAGE->set_course($course);
 
