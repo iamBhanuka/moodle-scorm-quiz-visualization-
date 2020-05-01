cd62c4f0342a546a68d56919b29453639f5eef4f
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-05-02 02:38:22 +0530

    lang-en

---
+++ tracker2/lang/en/block_tracker2.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'Simple tracker2 block';
+$string['tracker2'] = 'Simple tracker2';
+$string['overview']='Access tracker2';
+$string['tracker2:addinstance'] = 'Add a new simple tracker2 block';
+$string['tracker2:myaddinstance'] = 'Add a new simple tracker2 block to the My Moodle page';
