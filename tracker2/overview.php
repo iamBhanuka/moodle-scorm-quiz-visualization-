4737f727be0f71798f44421f3689ab71757cb44f
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-05-02 02:52:21 +0530

    commit

--- tracker2/overview.php
+++ tracker2/overview.php
@@ -35,11 +35,7 @@
     )
 );
 
-   $PAGE    ->  set_context($context);
-   $title = 'Time Spent per Lesson';
-   $PAGE    ->  set_title($title);      //sets title in title-bar
-   $PAGE    ->  set_heading($title);    //sets title in header
-   $PAGE    ->  navbar->add($title);    //adds title to navbar
+
   
    require_login($course, true);
 
