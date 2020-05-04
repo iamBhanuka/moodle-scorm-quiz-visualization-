a7a61b718c82c5ef9a1b267f3860c740510a570a
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-05-04 13:04:28 +0530

    fourth plugin

---
+++ viewsblock/lang/en/block_viewsblock.php
@@ -0,0 +1,7 @@
+<?php
+
+    $string['pluginname']='Views of resources';
+    $string['viewsblock']='All Views';
+    $string['overview']='overview of students';
+    $string['viewsblock:addinstance']='Add a new Views block';
+    $string['viewsblock:myaddinstance']='Add a new Views block to the my moodle page';
