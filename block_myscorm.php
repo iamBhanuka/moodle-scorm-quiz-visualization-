7ae26cf776f91b89f986570a52e493470a581419
Bhanuka Praneeth <bhanuka9519@gmail.com>
2020-05-05 16:52:24 +0530

    add public method

--- block_myscorm.php
+++ block_myscorm.php
@@ -6,3 +6,21 @@ class block_myscorm extends block_base {
     public function init() {
         $this->title = 'Courses scorm data';
     }
+    public function get_content() {
+        global $USER, $COURSE,$OUTPUT;
+
+        if ($this->content !== null) {
+          return $this->content;
+        }
+     
+        $this->content         =  new stdClass;
+        $this->content->text   = '';
+        $this->content->footer = '';
+
+        $parameters = array('myscormid' => $this->instance->id, 'courseid' => $COURSE->id, 'userid' => $USER->id);
+        $options = array('class' => 'OverviweButton');
+        $url = new moodle_url('/blocks/myscorm/overview.php',$parameters);
+        $this->content->text .= $OUTPUT->single_button($url, 'overviwe of students', 'post',$options);
+     
+        return $this->content;
+    }
