f822bf6b8fd37420e90781d586137fe45a18d151
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-05-05 07:41:29 +0530

    commit

---
+++ viewsblock/block_viewsblock.php
@@ -0,0 +1,31 @@
+<?php
+
+    require_once(dirname(__FILE__) . '/../../config.php');
+    
+    class block_viewsblock extends block_base {
+
+        public function init(){
+            $this->title= 'Views of resourses';
+        }
+
+        public function get_content(){
+            global $USER, $COURSE,$OUTPUT;
+
+            if($this->content!==null){
+                return $this->content;
+            }
+
+            $this->content=new stdClass;
+            $this->content->text='';
+            $this->content->footer='';           
+            
+            $parameters=array('viewsgraphid'=>$this->instance->id,'userid'=>$USER->id,'courseid'=>$COURSE->id);
+             $options = array('class' => 'overviewButton');
+             $url = new moodle_url('/blocks/viewsblock/overview.php',$parameters);        
+             $this->content->text .= $OUTPUT->single_button($url, 'overview of students', 'post',$options);
+            return $this->content;
+        }
+
+       
+    }
+    
