d84bac1ce44c646a955ede3fad6755686e7543f1
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-05-08 09:48:26 +0530

    add new funtion named block_graph_get_academic_year_id

--- studentaction/dropdown.php
+++ studentaction/dropdown.php
@@ -24,7 +24,7 @@ function block_graph_get_course_id($course, $year_id){
     return $course_id;
 }
 
-// function block_graph_get_academic_year_id($academic_year,$course_id){
+ function block_graph_get_academic_year_id($academic_year,$course_id){
 //     global $DB;
 //     $course_c = $DB->get_records_sql("SELECT id FROM {course_categories} WHERE name='$academic_year' AND parent ='$course_id'");
 //     foreach($course_c as $record_r=>$new_n)
@@ -32,7 +32,7 @@ function block_graph_get_course_id($course, $year_id){
 //         $academi_year_id=$new_n->id;
 //     }
 //     return $academi_year_id;
-// }
+ }
 
 // function block_graph_get_semester_id($semester,$academic_year_id){
 //     global $DB;
