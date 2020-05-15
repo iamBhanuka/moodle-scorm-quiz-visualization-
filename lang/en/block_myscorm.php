e2ccca2d2f62202f8c15d06a3265441d09b80990
Bhanuka Praneeth <bhanuka9519@gmail.com>
2020-05-15 17:20:53 +0530

    create html data

--- lang/en/block_myscorm.php
+++ lang/en/block_myscorm.php
@@ -2,3 +2,5 @@
 $string['pluginname'] = ' myscorm 1 Block';
 $string['myscorm'] = 'myscorm 1';
 $string['blockstring'] = 'BlockString';
+$string['myscorm:addinstance'] = 'Add a new simple HTML block';
+$string['myscorm:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
