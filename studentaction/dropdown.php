3c7bf92d500082083da11712277c05595e727425
unknown <SanduniAbenayaka>
2020-05-15 10:00:19 +0530

    get ids

--- studentaction/dropdown.php
+++ studentaction/dropdown.php
@@ -40,7 +40,7 @@ function block_graph_get_semester_id($semester,$academic_year_id){
     $course_sem = $DB->get_records_sql("SELECT id FROM {course_categories} WHERE name='$semester' AND parent ='$academic_year_id'");
     foreach($course_sem as $record_r=>$new_n)
     {
-     $semester_id=$new_n->id;
+        $semester_id=$new_n->id;
     
     }
     return $semester_id;
