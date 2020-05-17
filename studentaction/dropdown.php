82293d5ada620b821780ac88eb45e0cde984e81c
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-05-17 10:03:50 +0530

    add foreach

--- studentaction/dropdown.php
+++ studentaction/dropdown.php
@@ -70,10 +70,10 @@ function get_course_names($semester_id ){
    
     $course=$DB->get_records_sql("SELECT fullname,startdate,shortname FROM {course} WHERE category ='$semester_id'"); 
    
-    // foreach ($course as $c=>$fullname) {
+    foreach ($course as $c=>$fullname) {
     //         $label[$i] =$fullname->fullname;
     //         $i++;              
-    // }
+    }
   
     //return $label;
 };
