307cafe123a277ef52d14ca63c6987ba093d573b
amnd1188 <amandadasanayaka4336@gmail.com>
2020-05-18 17:34:40 +0530

    define some values

--- overview.php
+++ overview.php
@@ -2,3 +2,7 @@
 
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->dirroot.'/blocks/myscorm/lib.php');
+$extention_array = array('jpg','png','jpeg','gif','ico');
+define('USER_SMALL_CLASS', 20);   
+define('USER_LARGE_CLASS', 200);  
+define('DEFAULT_PAGE_SIZE', 20);
