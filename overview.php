907003c1d573a9f2f69a8350e4e0d659d496f5e9
amnd1188 <amandadasanayaka4336@gmail.com>
2020-05-19 17:35:40 +0530

    required_param

--- overview.php
+++ overview.php
@@ -6,3 +6,7 @@ $extention_array = array('jpg','png','jpeg','gif','ico');
 define('USER_SMALL_CLASS', 20);   
 define('USER_LARGE_CLASS', 200);  
 define('DEFAULT_PAGE_SIZE', 20);
+define('SHOW_ALL_PAGE_SIZE', 5000);
+
+$id       = required_param('myscormid', PARAM_INT);
+$courseid = required_param('courseid', PARAM_INT);
