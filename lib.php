8be0ff1cbc95613d0db204ccbc212e9f1964c3d6
Bhanuka Praneeth <bhanuka9519@gmail.com>
2020-05-19 17:54:23 +0530

    add add block scorm caurse function

--- lib.php
+++ lib.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../../config.php');
+function block_myscorm_course_context($courseid) { //get login page data
+    if (class_exists('context_course')) {
+        return context_course::instance($courseid);
+    } else {
+        return get_context_instance(CONTEXT_COURSE, $courseid);
+    }
+}
