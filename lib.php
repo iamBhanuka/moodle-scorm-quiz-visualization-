c92af646e40e9fba4f216de0e8e765a56d412032
Bhanuka Praneeth <bhanuka9519@gmail.com>
2020-05-22 17:56:31 +0530

    write sql quary

--- lib.php
+++ lib.php
@@ -12,3 +12,7 @@ function block_myscorm_course_context($courseid) { //get login page data
 function get_login_datas($courseid,$userid ){
 
     global $DB;
+
+    $sql_courses =  "SELECT C.* FROM {role_assignments} as A INNER JOIN {context} as B on A.contextid=B.id INNER JOIN {course} as C on C.id=B.instanceid AND B.contextlevel=50 AND A.roleid='3'  AND A.userid='$userid' ;";
+    $sql_courses_res = $DB->get_records_sql($sql_courses);
+    $has_course = false;
