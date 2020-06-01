43f8482215b7fdc2d5aba86e8299699391e78f8d
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-06-02 04:13:28 +0530

    modified

--- coursegraph/block_coursegraph.php
+++ coursegraph/block_coursegraph.php
@@ -1,47 +1,47 @@
 <?php
 
-// require_once(dirname(__FILE__) . '/../../config.php');
+require_once(dirname(__FILE__) . '/../../config.php');
 
 
-// class block_coursegraph extends block_base {
+class block_coursegraph extends block_base {
 
-//     public function init(){
-//         $this->title='Course Overview';
-//     }
+    public function init(){
+        $this->title='Course Overview';
+    }
 
-//     public function specialization(){
-//         if(isset($this->config)){
+    public function specialization(){
+        if(isset($this->config)){
            
-//         }
-//    }
+        }
+   }
 
-//    public function instance_allow_multiple(){
-//         return true;
-//    }
+   public function instance_allow_multiple(){
+        return true;
+   }
 
-//    function has_config(){
-//          return true;
-//    }
+   function has_config(){
+         return true;
+   }
 
-//    public function applicable_formats(){
-//        return array(
-//            'course-view'    => true,  
-//            'site'           => true,
-//            'mod'            => false,
-//            'my'             => true        
-//        );
-//    }
+   public function applicable_formats(){
+       return array(
+           'course-view'    => true,  
+           'site'           => true,
+           'mod'            => false,
+           'my'             => true        
+       );
+   }
 
-//     public function get_content(){
-//         global $USER, $COURSE, $CFG, $OUTPUT, $DB;
+    public function get_content(){
+        global $USER, $COURSE, $CFG, $OUTPUT, $DB;
 
-//          if($this->content!==null){
-//              return $this->content;
-//          }
+         if($this->content!==null){
+             return $this->content;
+         }
 
-//          $this->content=new stdClass;
-//          $this->content->text='';
-//          $this->content->footer='';
+         $this->content=new stdClass;
+         $this->content->text='';
+         $this->content->footer='';
          
        
 //          $parameters=array( 'coursegraphid' => $this->instance->id,'courseid' => $COURSE->id,'userid'=>$USER->id);
@@ -53,9 +53,9 @@
 //          $this->content->text .= $OUTPUT->single_button($url2, 'My Marks', 'post',$options2);
          
 //          return $this->content;
-//     }
+     }
 
     
       
-// }
+}
  
