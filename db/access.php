de47ee8e449a33b48799391ea74a2a2186174e7d
amnd1188 <amandadasanayaka4336@gmail.com>
2020-06-01 16:33:24 +0530

    edite db

--- db/access.php
+++ db/access.php
@@ -13,3 +13,14 @@
 
     'block/myscorm:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
+  'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
+
