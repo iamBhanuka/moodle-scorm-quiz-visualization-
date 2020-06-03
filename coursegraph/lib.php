76a0403151401b0fd8f708bd5a88ad61a966a805
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-06-04 04:19:48 +0530

    second update

--- coursegraph/lib.php
+++ coursegraph/lib.php
@@ -36,63 +36,63 @@ function block_coursegraph_course_context($courseid) { //get login page data
 // }
 
 
-// function get_activity_details($userid,$courseid){
-//     global  $USER, $COURSE, $CFG, $OUTPUT, $DB,$countu;
-//     $k=0;
-//     $chart = new \core\chart_line();
-//     $course_loga=get_date_options();
+function get_activity_details($userid,$courseid){
+    global  $USER, $COURSE, $CFG, $OUTPUT, $DB,$countu;
+    $k=0;
+    $chart = new \core\chart_line();
+    $course_loga=get_date_options();
     
-//     $subjects=block_graph_get_enrol_course($userid,$courseid);
-//     for($r=0;$r<count($subjects);$r++)
-//     {
-//          $subjects[$r] ;
-//     }
-//     $names=get_course_names($subjects);
-//     for($r=0;$r<count($names);$r++)
-//     {
-//          $names[$r] ;
-//     }
+    $subjects=block_graph_get_enrol_course($userid,$courseid);
+    for($r=0;$r<count($subjects);$r++)
+    {
+         $subjects[$r] ;
+    }
+    $names=get_course_names($subjects);
+    for($r=0;$r<count($names);$r++)
+    {
+         $names[$r] ;
+    }
     
-//     foreach($subjects as $list)
-//         {
-//             echo '<br>';
-//             $x=0;
+    // foreach($subjects as $list)
+    //     {
+    //         echo '<br>';
+    //         $x=0;
             
-//             foreach( $course_loga as $da)
-//             {
-//                    $sql5= "SELECT  COUNT(action) AS 'countu',courseid
-//                             FROM {logstore_standard_log} 
-//                             WHERE  userid=$userid  AND action='viewed' AND courseid =$list AND DATE_FORMAT(FROM_UNIXTIME(timecreated),'%D %M %Y') ='$da'
-//                           "; 
-//                     $login5=$DB->get_records_sql($sql5);
-//                     foreach($login5 as $record_r=>$new_n)
-//                         {
-//                             $c_id[$x]=$new_n->countu;   
-//                         } 
-//                         $x++;
+    //         foreach( $course_loga as $da)
+    //         {
+    //                $sql5= "SELECT  COUNT(action) AS 'countu',courseid
+    //                         FROM {logstore_standard_log} 
+    //                         WHERE  userid=$userid  AND action='viewed' AND courseid =$list AND DATE_FORMAT(FROM_UNIXTIME(timecreated),'%D %M %Y') ='$da'
+    //                       "; 
+    //                 $login5=$DB->get_records_sql($sql5);
+    //                 foreach($login5 as $record_r=>$new_n)
+    //                     {
+    //                         $c_id[$x]=$new_n->countu;   
+    //                     } 
+    //                     $x++;
 
-//             }
+    //         }
             
           
-//             $series = new \core\chart_series($names[$k], $c_id);
-//             $chart->add_series($series);
-//             $k++;
-//             if($max<=max($c_id)){
-//                 $max= max($c_id);
-//             }
+    //         $series = new \core\chart_series($names[$k], $c_id);
+    //         $chart->add_series($series);
+    //         $k++;
+    //         if($max<=max($c_id)){
+    //             $max= max($c_id);
+    //         }
             
-//         }
+    //     }
 
-// $chart->set_labels($course_loga);
-// $yaxis = $chart->get_yaxis(0, true);
-// $yaxis->set_label('number of actoins');
-// $yaxis->set_stepsize(max(1, round($max / 10)));
+$chart->set_labels($course_loga);
+$yaxis = $chart->get_yaxis(0, true);
+$yaxis->set_label('number of actoins');
+$yaxis->set_stepsize(max(1, round($max / 10)));
 
-// echo $OUTPUT->render($chart);
+echo $OUTPUT->render($chart);
  
 
 
-// }
+}
 
 
 
