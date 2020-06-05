ca305126897f3936da7b648448265d00bc524de1
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-06-06 04:22:06 +0530

    first update

--- coursegraph/showaccess.php
+++ coursegraph/showaccess.php
@@ -27,66 +27,66 @@
 // }
 
 
-// function block_graph_get_enrol_course($userid,$courseid){
-//     global $DB,$course,$instance,$CFG,$semester_id,$course_elist;
-//     $userid=$userid;
-//     $course_id=$courseid;
-//     $label=array();
-//     $course_elist=array();
-//     $x=0;
-//     $i=0;
-//     //echo "sfgg";
-//     // $course_sem = $DB->get_records_sql("SELECT id,shortname FROM {role} ");
-//     // foreach($course_sem as $record_r=>$new_n)
-//     // {
-//     //     echo $semester_id=$new_n->id."........".$new_n->shortname;
+function block_graph_get_enrol_course($userid,$courseid){
+    global $DB,$course,$instance,$CFG,$semester_id,$course_elist;
+    $userid=$userid;
+    $course_id=$courseid;
+    $label=array();
+    $course_elist=array();
+    $x=0;
+    $i=0;
+    //echo "sfgg";
+    // $course_sem = $DB->get_records_sql("SELECT id,shortname FROM {role} ");
+    // foreach($course_sem as $record_r=>$new_n)
+    // {
+    //     echo $semester_id=$new_n->id."........".$new_n->shortname;
     
-//     // }
+    // }
 
-//     $sql1="SELECT * FROM {role_assignments} WHERE  roleid='5' AND userid='$userid';";
-//     $role = $DB->get_records_sql($sql1);
-//     foreach ($role as $renew=>$new)
-//     {
-//         $contextid1= $new->contextid;
-//         $instance= $DB->get_records_sql("SELECT instanceid FROM {context} WHERE id='$contextid1' AND contextlevel='50'");
-//         foreach($instance as $record=> $new ){
-//             $instanceid = $new->instanceid.'<br>';
-//             $course=$DB->get_records_sql("SELECT id,fullname,startdate,shortname FROM {course} WHERE id ='$instanceid'");
-//             foreach($course as $record=>$newid){
-//                 $course_elist[$x]=$newid->id;
-//                 $course_elist[$x].'<br>';
-//                 $x++;
-//             }
-//         } 
-//     }  
-//     //return $course_elist;
-//     $course = $DB->get_records_sql("SELECT category FROM {course} WHERE id='$course_id'");
+    $sql1="SELECT * FROM {role_assignments} WHERE  roleid='5' AND userid='$userid';";
+    $role = $DB->get_records_sql($sql1);
+    foreach ($role as $renew=>$new)
+    {
+        $contextid1= $new->contextid;
+        $instance= $DB->get_records_sql("SELECT instanceid FROM {context} WHERE id='$contextid1' AND contextlevel='50'");
+        foreach($instance as $record=> $new ){
+            $instanceid = $new->instanceid.'<br>';
+            $course=$DB->get_records_sql("SELECT id,fullname,startdate,shortname FROM {course} WHERE id ='$instanceid'");
+            foreach($course as $record=>$newid){
+                $course_elist[$x]=$newid->id;
+                $course_elist[$x].'<br>';
+                $x++;
+            }
+        } 
+    }  
+    //return $course_elist;
+    $course = $DB->get_records_sql("SELECT category FROM {course} WHERE id='$course_id'");
     
-//     foreach ($course as $c=>$fullname) {
-//         $semester_id =$fullname->category; 
-//         $semester_id.'<br>'; 
+    foreach ($course as $c=>$fullname) {
+        $semester_id =$fullname->category; 
+        $semester_id.'<br>'; 
                    
-//     }
-//     foreach($course_elist as $list)
-//     {
-//         $course_loga=$DB->get_records_sql("SELECT id,fullname,startdate,shortname FROM {course} WHERE category='$semester_id'  AND 
-//         id =$list");
+    }
+    foreach($course_elist as $list)
+    {
+        $course_loga=$DB->get_records_sql("SELECT id,fullname,startdate,shortname FROM {course} WHERE category='$semester_id'  AND 
+        id =$list");
 
-//     /*$course_loga=$DB->get_records_sql("SELECT id,fullname,startdate,shortname FROM {course} WHERE category='$semester_id' AND 
-//     id IN 
-//     (SELECT id,fullname,startdate,shortname FROM {course} WHERE contextlevel='50' AND id IN
-//     (SELECT instanceid FROM {context} WHERE id IN 
-//     (SELECT * FROM {role_assignments} WHERE  roleid='5' AND userid='$userid')  )) "); */
+    /*$course_loga=$DB->get_records_sql("SELECT id,fullname,startdate,shortname FROM {course} WHERE category='$semester_id' AND 
+    id IN 
+    (SELECT id,fullname,startdate,shortname FROM {course} WHERE contextlevel='50' AND id IN
+    (SELECT instanceid FROM {context} WHERE id IN 
+    (SELECT * FROM {role_assignments} WHERE  roleid='5' AND userid='$userid')  )) "); */
    
-//         foreach ($course_loga as $c=>$fullname) {
-//                     //$label[$i] =$fullname->fullname;
-//             $label[$i] =$fullname->id;
+        foreach ($course_loga as $c=>$fullname) {
+                    //$label[$i] =$fullname->fullname;
+            $label[$i] =$fullname->id;
 
-//             $i++;              
-//         }
-//     }
-//     return $label;
-// }
+            $i++;              
+        }
+    }
+    return $label;
+}
 
 // function get_course_names($subjects){  
 
