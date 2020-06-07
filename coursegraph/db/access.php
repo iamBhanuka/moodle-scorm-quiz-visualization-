eb5a68f2cbf44d28fbf5ecdb61c370202758117e
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-06-08 04:29:28 +0530

    modified access file

--- coursegraph/db/access.php
+++ coursegraph/db/access.php
@@ -1,29 +1,29 @@
 <?php
-    // $capabilities = array(
-    //     'block/coursegraph:myaddinstance' => array(
-    //         'riskbitmask' => RISK_PERSONAL,
-    //         'captype' => 'read',
-    //         'contextlevel' => CONTEXT_SYSTEM,
-    //         'archetypes' => array(
-    //             'user' => CAP_ALLOW,
-    //         ),
+    $capabilities = array(
+        'block/coursegraph:myaddinstance' => array(
+            'riskbitmask' => RISK_PERSONAL,
+            'captype' => 'read',
+            'contextlevel' => CONTEXT_SYSTEM,
+            'archetypes' => array(
+                'user' => CAP_ALLOW,
+            ),
     
         
             
-    //         'clonepermissionsfrom'=>'moodle/my:manageblocks'
-    //     ),
+            'clonepermissionsfrom'=>'moodle/my:manageblocks'
+        ),
 
-    //     'block/coursegraph:addinstance'=>array(
-    //         'riskbitmask'=> RISK_SPAM | RISK_XSS,
+        'block/coursegraph:addinstance'=>array(
+            'riskbitmask'=> RISK_SPAM | RISK_XSS,
 
-    //         'captype'=>'write',
-    //         'contextlevel'=>CONTEXT_BLOCK,
-    //         'archetypes'=>array(
-    //             'editingteacher'=>CAP_ALLOW,
-    //             'manager'=>CAP_ALLOW
-    //         ),
+            'captype'=>'write',
+            'contextlevel'=>CONTEXT_BLOCK,
+            'archetypes'=>array(
+                'editingteacher'=>CAP_ALLOW,
+                'manager'=>CAP_ALLOW
+            ),
 
-    //         'clonepermissionsfrom'=>'moodle/site:manageblocks'
-    //     ),
+            'clonepermissionsfrom'=>'moodle/site:manageblocks'
+        ),
 
-    // );
+    );
