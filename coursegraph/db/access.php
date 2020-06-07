55fcc870f4a2587652217d1a50a2e1576e647f8b
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-06-08 04:29:02 +0530

    add access file

---
+++ coursegraph/db/access.php
@@ -0,0 +1,29 @@
+<?php
+    // $capabilities = array(
+    //     'block/coursegraph:myaddinstance' => array(
+    //         'riskbitmask' => RISK_PERSONAL,
+    //         'captype' => 'read',
+    //         'contextlevel' => CONTEXT_SYSTEM,
+    //         'archetypes' => array(
+    //             'user' => CAP_ALLOW,
+    //         ),
+    
+        
+            
+    //         'clonepermissionsfrom'=>'moodle/my:manageblocks'
+    //     ),
+
+    //     'block/coursegraph:addinstance'=>array(
+    //         'riskbitmask'=> RISK_SPAM | RISK_XSS,
+
+    //         'captype'=>'write',
+    //         'contextlevel'=>CONTEXT_BLOCK,
+    //         'archetypes'=>array(
+    //             'editingteacher'=>CAP_ALLOW,
+    //             'manager'=>CAP_ALLOW
+    //         ),
+
+    //         'clonepermissionsfrom'=>'moodle/site:manageblocks'
+    //     ),
+
+    // );
