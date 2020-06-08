89ff754419d0dc0987672ac66293ad3cbf180a64
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-06-09 04:31:28 +0530

    changed overview

--- coursegraph/lang/en/block_coursegraph.php
+++ coursegraph/lang/en/block_coursegraph.php
@@ -1,6 +1,6 @@
 <?php
 $string['pluginname']='Course Graph block';
 $string['coursegraph']='My Coursegraph Block';
-$string['overview']='overview of students result';
+$string['overview']='overview of student ';
 $string['coursegraph:addinstance']='Add a new coursegraph block';
 $string['coursegraph:myaddinstance']='Add a new coursegraph block to the my moodle page';
