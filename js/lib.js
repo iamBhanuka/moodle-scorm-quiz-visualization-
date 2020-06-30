3f7e5a19c419fd4c2f81fee2d1e0ac0303f77b3a
Bhanuka Praneeth <bhanuka9519@gmail.com>
2020-06-30 18:19:50 +0530

    creat js quary 2

--- js/lib.js
+++ js/lib.js
@@ -16,3 +16,28 @@ scormData.forEach((e) => {
     sorted.push(e);
   }
 });
+
+function scormSelect() {
+    var x = document.getElementById("dd_scorm").value;
+    var scormDataDiv = document.getElementById("scorm_data");
+    scormDataDiv.innerHTML = "";
+  
+    sorted
+      .filter((data) => {
+        return data.scorm_id == x;
+      })
+      .forEach((data) => {
+        scormDataDiv.innerHTML +=
+          "id: " +
+          data.userid +
+          " name: " +
+          data.firstname +
+          " " +
+          data.lastname +
+          " score: " +
+          data.mark +
+          " attempt: " +
+          data.attempt +
+          "</br>";
+      });
+  }
