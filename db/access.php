4e06d207466299edb7813146e1173f79035ed635
Bhanuka Praneeth <bhanuka9519@gmail.com>
2020-07-07 16:20:48 +0530

    create bd folder

---
+++ db/access.php
@@ -0,0 +1,12 @@
+<?php
+    $capabilities = array(
+ 
+    'block/myscorm:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
