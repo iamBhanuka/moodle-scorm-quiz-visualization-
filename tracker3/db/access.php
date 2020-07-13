554eec9dc5f32f4d07dcaa8a9d69941b3853fb97
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-13 12:33:43 +0530

    commit

--- tracker3/db/access.php
+++ tracker3/db/access.php
@@ -1,7 +1,7 @@
 <?php
     $capabilities = array(
  
-    'block/tracker:myaddinstance' => array(
+    'block/tracker3:myaddinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
@@ -11,7 +11,7 @@
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
  
-    'block/tracker:addinstance' => array(
+    'block/tracke3:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
  
         'captype' => 'write',
