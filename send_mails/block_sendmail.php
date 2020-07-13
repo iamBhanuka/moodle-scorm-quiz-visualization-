b124981d0c2ef208ed73f6c732c8b2e5a2044ed9
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-07-13 18:53:24 +0530

    block

--- send_mails/block_sendmail.php
+++ send_mails/block_sendmail.php
@@ -5,7 +5,7 @@
     class block_sendmail extends block_base {
 
         public function init(){
-            $this->title= 'Send mails';
+            $this->title= 'Time remaining';
         }
 
         public function get_content(){
@@ -21,8 +21,8 @@
         
             $parameters=array('sendmailid'=>$this->instance->id,'userid'=>$USER->id,'courseid'=>$COURSE->id);
             $options = array('class' => 'overviewButton'); 
-            $url = new moodle_url('/blocks/send_mail/overview.php',$parameters);        
-            $this->content->text .= $OUTPUT->single_button($url, 'overview of students', 'post',$options);
+            $url = new moodle_url('/blocks/sendmail/overview.php',$parameters);        
+            $this->content->text .= $OUTPUT->single_button($url, 'overview of time', 'post',$options);
             return $this->content;
         }
 
