599327d97a2dd02f6cdd244da387bdfca51ccafb
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-14 13:41:27 +0530

    commit

--- tracker3/overview.php
+++ tracker3/overview.php
@@ -56,26 +56,8 @@
                 AND courseid=$courseid) 
             ORDER BY userid DESC;";
     $result_time = $DB->get_records_sql($time);
-    // echo '<pre>'; print_r($result_time); echo '</pre>';
 
-// -----------------------------
-//    https://www.tutorialspoint.com/return-all-dates-between-two-dates-in-an-array-in-php#:~:text=php%20function%20displayDates(%24date1%2C,('2019%2D11%2D10
-//    function displayDates($date1, $date2, $format = 'd-m-Y' ) {
-//       $dates = array();
-//       $current = strtotime($date1);
-//       $date2 = strtotime($date2);
-//       $stepVal = '+1 day';
-//       while( $current <= $date2 ) {
-//          $dates[] = date($format, $current);
-//          $current = strtotime($stepVal, $current);
-//       }
-//       return $dates;
-//    }
-//    $date = displayDates('2019-11-10', '2019-11-20');
-//    var_dump($date);
-// --------------------------------
-
-    function displayDates($date1, $date2, $format = 'd-m-Y' ) {
+    function displayDates($date1, $date2, $format = 'd-m-y' ) {
         $dates = array();
         $current = strtotime($date1);
         $date2 = strtotime($date2);
@@ -87,141 +69,17 @@
         return $dates;
      }
 
-/*<<<<<<< HEAD
-     //$date = displayDates(date("d-m-Y", strtotime("-1 months")), date("d-m-Y"));
-     //echo '<pre>'; print_r($date); echo '</pre>';
-=======
-     $date = displayDates(date("d-m-Y", strtotime("-1 months")), date("d-m-Y"));
-     echo '<pre>'; print_r($date); echo '</pre>';
->>>>>>> b1e319eb510a23cac0aff88ae7cbabf1e8d026cb*/
-
-    //  $date = displayDates('2019-2-25', '2019-3-2');
-    //  echo '<pre>'; print_r($date); echo '</pre>';
-    //  $date = displayDates('2020-2-25', '2020-3-2');
-    //  echo '<pre>'; print_r($date); echo '</pre>';
-     //$date=array("2-3-2020", "2-4-2020", "2-5-2020");
-     //echo '<pre>'; print_r($date); echo '</pre>';
-
-    //  date("d-m-Y")
-    //  var_dump($date);
-
-        //  create a new chart
     $chart = new \core\chart_line();
     //name its axis
     $chart->get_xaxis(0, true)->set_label("Days"); 
     $chart->get_yaxis(0, true)->set_label("Time spent per lesson(hrs)");
-    $time=array(2, 4, 10);
+    $time=array(6, 4, 10);
     //$time=array(2, 4, 10, 3, 2, 3, 4, 3, 4, 3, 4, 5, 9, 5, 6, 2, 23, 4, 3, 23, 1, 4, 3, 5, 3, 5, 6, 11, 2, 3);
     $time_per_student = new core\chart_series("time", $time);
     $chart->add_series($time_per_student);
-/*<<<<<<< HEAD
     $date=array("16-6-2020", "13-7-2020", "14-7-2020");
-=======
-    $date=array("16-6-2020", "13-7-2020", "14-7-2020");
->>>>>>> b1e319eb510a23cac0aff88ae7cbabf1e8d026cb*/
     $chart->set_labels($date);
     echo $OUTPUT->render($chart);
-
-    // ------------------------------------------
-    // $chart = new \core\chart_line();
-    // $serie1 = new core\chart_series('My series title', [509, 1000, 127, 540]);
-    // $chart->add_series($serie1);
-    // $serie2 = new core\chart_series('My series title', [400, 460, 1120, 540]);
-    // $chart->add_series($serie2);
-    // $chart->set_labels(['2004', '2005', '2006', '2007']);
-    // //$chart->set_labels($labels);
-    // echo $OUTPUT->render($chart);
-    // --------------------------------------------
-
-//     //connect scormid and scoid
-//     $join_scorm_and_scoes = "SELECT id, scorm FROM {scorm_scoes};";
-//     $joined = $DB->get_records_sql($join_scorm_and_scoes);
-
-//     //getting lesson names from db
-//     $sco_lessons = "SELECT id, name FROM {scorm} WHERE course = $courseid";
-//     $info_sco_lessons = $DB->get_records_sql($sco_lessons);
-
-//     //initialize name list for scorm lessons
-//     $name = array();
-//     foreach($info_sco_lessons as $sco_name){
-//         //entering lesson names into array by id
-//         array_push($name, $sco_name->name);
-//     }
-
-//     //get name of course
-//     $fullname = "SELECT fullname FROM {course} WHERE id=$courseid";
-//     $coursename = $DB->get_records_sql($fullname);
-//     foreach($coursename as $info_coursename){
-//         $course_name=$info_coursename->fullname;
-//     }
-
-//     //create a new chart
-//     $chart = new \core\chart_line();
-//     //name its axis
-//     $chart->get_xaxis(0, true)->set_label("Days in ". $course_name); 
-//     $chart->get_yaxis(0, true)->set_label("Time spent per lesson(hrs)");
-//     //$chart->set_smooth(true); // Calling set_smooth() passing true as parameter, will display smooth lines.
-
-//     //get ids, names of students enrolled in course
-//     $contextid = get_context_instance(CONTEXT_COURSE, $courseid);
-//     $users = "SELECT u.id, u.username
-//                 FROM {user} u, {role_assignments} r
-//                 WHERE u.id=r.userid
-//                     AND r.contextid = {$contextid->id}";
-//     $info_students = $DB->get_records_sql($users);
-
-//     $sc=0;
-//     $sc1=0;
-
-//     $stu_name = array();
-
-//     foreach($info_students as $user_info){
-
-//         //entering user names into array by id
-//         $stu_name[$user_info->id]=$user_info->username;
-
-//         $access_array=array();
-
-//         //find which scorm packages each student has accessed
-//         $sql = "SELECT sst.scormid, sst.scoid, sst.value 
-//         FROM {scorm_scoes_track} sst, {scorm} s 
-//         WHERE sst.scormid=s.id 
-//             AND element='cmi.core.total_time' 
-//             AND sst.userid=$user_info->id 
-//             AND s.course=$courseid;";
-//         $result = $DB->get_records_sql($sql);
-
-//         //fill array if student hasn't accessed a scorm pkg
-//         foreach($info_sco_lessons as $value){
-//             if (!isset($result[$value->id])){
-//                 $result[$value->id]->value=0;
-//             }
-//         }
-//         ksort($result); //sort array by key
-
-//         $sc=0;
-
-//         //expand [value] in $result to convert 00:00:00.00 into hours
-//         foreach($result as $value){
-//             $split_time_value = explode (":", $value->value);
-//             $split_time_value[3]=($split_time_value[0])+($split_time_value[1]/60)+($split_time_value[2]/(60*60));
-//             if ($split_time_value[3]>0){
-//                 $access_array[$sc]=$split_time_value[3];
-//             }
-//             else{
-//                 $access_array[$sc]=0;
-//             }
-//             $sc++;
-//         }
-//         // echo '<pre>'; print_r($access_array); echo '</pre>';
-
-//         //sets line-chart lines to each student
-//         $time_per_student = new core\chart_series($user_info->username, $access_array);
-//         $chart->add_series($time_per_student);
-//     }
-
-//     $chart->set_labels($name);
-//     echo $OUTPUT->render($chart);
 
     echo '</div>';
 
