56cbd02a1646158a3b19e9dced7296e7638986a8
AanikaP <43165703+AanikaP@users.noreply.github.com>
2020-07-25 01:29:39 +0530

    updated overview file. old overview is now overview-old1

--- tracker3/overview.php
+++ tracker3/overview.php
@@ -48,40 +48,218 @@
    echo $OUTPUT->container_start('block_tracker3');
    echo '<div>';
 
-    global $DB;
+   global $DB;
+    
+   $oneMonthAgo = date("d-m-Y", strtotime("-2 months"));
+   $today = date("d-m-Y");
 
-    $time="SELECT id, FROM_UNIXTIME(timecreated, '%d-%m-%y') as timecreated, userid, objectid/2
-            FROM {logstore_standard_log} 
-            WHERE ((eventname LIKE '%sco_launched' OR eventname LIKE '%content_pages_viewed')
-                AND courseid=$courseid) 
-            ORDER BY userid DESC;";
-    $result_time = $DB->get_records_sql($time);
+   //connect scormid and scoid
+   // $sql = "SELECT  `mdl_scorm_scoes`.`id` as ss_id, `mdl_scorm`.`id` as s_id, `mdl_scorm_scoes`.`scorm`, `mdl_scorm`.`name` FROM `mdl_scorm`, `mdl_scorm_scoes` WHERE `mdl_scorm`.`id`=`mdl_scorm_scoes`.`scorm` AND `mdl_scorm`.`course`=16";
+   $join_scorm_and_scoes = "SELECT  ss.id as sco_id, ss.scorm, s.name as sco_name
+                           FROM {scorm} as s, {scorm_scoes} as ss 
+                           WHERE s.id=ss.scorm 
+                                   AND s.course=$courseid;";
+   $joined = $DB->get_records_sql($join_scorm_and_scoes);
+   //echo '<pre>'; print_r($joined); echo '</pre>';
 
-    function displayDates($date1, $date2, $format = 'd-m-y' ) {
+if (empty($joined)){ echo "No scorm packages for this course.";    }
+
+else {
+
+    //is index valid
+    $doIExist=0;
+    //Getting the index
+    $index=0;
+    echo '<head>
+        <style>
+            input:focus{    outline: 2px solid purple;    }
+        </style>
+    </head>';
+
+    //start a form to get index from user
+    echo html_writer::start_tag('form', array('action' =>'overview.php', 'method' => 'post'));
+
+    //use table for neater formatting
+    echo '<table>';
+        echo '<tr>';
+            //get index as input
+            echo '<td>';
+                echo html_writer::empty_tag('input', array('type'=>'text', 'name'=>'id', 'autocomplete'=>'off', 'placeholder'=>' Enter student id ', 'style'=>'height:35px; width:150px; border:1px solid purple'));
+            echo '</td>';
+
+            //other prefixed inputs
+                echo html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'tracker3id', 'value'=>$id));
+                echo html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'courseid', 'value'=>$courseid));
+                echo html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'userid', 'value'=>$userid));
+            
+            //submit all inputs
+            echo '<td>';          
+                echo html_writer::empty_tag('input', array('type'=>'submit', 'class'=>'btn-primary', 'value'=> 'scorm access details', 'style'=>'height:35px; width:150px; border:1px; background-color:purple'));
+            echo '</td>';
+        echo '</tr>';
+    echo '</table>'; 
+
+    echo html_writer::end_tag('form').'<br>';
+
+    //get input to use in chart
+    $index= $_POST['id'];
+
+    function displayDates($date1, $date2, $format = 'd-m-Y' ) {
         $dates = array();
+        $array_dates=array();
         $current = strtotime($date1);
         $date2 = strtotime($date2);
         $stepVal = '+1 day';
         while( $current <= $date2 ) {
             array_push($dates, date($format, $current));
             $current = strtotime($stepVal, $current);
+            //echo "date ------------".strtotime(date($format, $current));
         }
         return $dates;
-     }
+    }
+    $date_find=displayDates($oneMonthAgo, $today);
+    //echo '<pre>'; print_r($date_find); echo '</pre>';
 
+    //create a new chart
     $chart = new \core\chart_line();
     //name its axis
     $chart->get_xaxis(0, true)->set_label("Days"); 
     $chart->get_yaxis(0, true)->set_label("Time spent per lesson(hrs)");
-    $time=array(0.2, 0.08, 0.01, 0.04, 0.08, 0.37, 0.9, 0.13, 1.2, 0.03, 0, 0.18);
-    //$time=array(2, 4, 10, 3, 2, 3, 4, 3, 4, 3, 4, 5, 9, 5, 6, 2, 23, 4, 3, 23, 1, 4, 3, 5, 3, 5, 6, 11, 2, 3);
-    $time_per_student = new core\chart_series("time", $time);
-    $chart->add_series($time_per_student);
-    $date=displayDates("16-6-2020", "27-6-2020");
-    $chart->set_labels($date);
-    echo $OUTPUT->render($chart);
 
-    echo '</div>';
+    //get ids, names of students enrolled in course
+    $contextid = get_context_instance(CONTEXT_COURSE, $courseid);
+    $users = "SELECT u.id, u.username
+                FROM {user} u, {role_assignments} r
+                WHERE u.id=r.userid
+                    AND r.contextid = {$contextid->id}
+                ORDER BY u.username";
+    $info_students = $DB->get_records_sql($users);
+    //echo '<pre>'; print_r($info_students); echo '</pre>';
+    //echo '<pre>'; print_r($info_students[2]->username); echo '</pre>';
+
+    $stu_name = array();
+
+    //check if index is valid
+    foreach($info_students as $user_info){
+        if ($user_info->username==$index){
+            $doIExist=1;
+            $index_id = $user_info->id;
+        }
+    }
+    //echo "id".$index_id;
+
+    //set heading if index valid
+    if ($doIExist==1 && $index!=NULL){
+        echo 'Access details of '.$index.': ';
+    }
+    //make sure index is valid
+    else if ($doIExist!=1 && $index!=NULL){
+        echo ' Index not valid';
+    }
+
+if ($doIExist==1 && $index!=NULL){
+    $get_timecreated = "SELECT timecreated, objectid, userid  
+                        FROM {logstore_standard_log} 
+                        WHERE eventname LIKE '%sco_launched' 
+                            AND courseid=$courseid
+                            AND userid=$index_id;";
+    $timestart = $DB->get_records_sql($get_timecreated);
+    // echo '<pre>'; print_r($timestart); echo '</pre>';
+
+    $array=array();
+    $c=0;
+    foreach ($timestart as $value){
+        $array[$c][sco_name]=$joined[$value->objectid]->sco_name;
+        $array[$c][stu_id]=$value->userid;
+        $array[$c][date]=date('d-m-Y', $value->timecreated);
+        $array[$c][time]=date('h:m:s', $value->timecreated);
+        $array[$c][timestart]=$value->timecreated;
+        //$array=date('m/d/Y', $value->timecreated)]=date('h:m:s', $value->timecreated);
+        //array_push($array, date('m/d/Y h:m:s', $value->timecreated));
+
+        $get_timestopped = "SELECT timecreated 
+                            FROM {logstore_standard_log} 
+                            WHERE timecreated>=$value->timecreated 
+                                AND (eventname LIKE '%course_viewed' 
+                                    OR `eventname` LIKE '%dashboard_viewed')
+                                AND courseid=$courseid
+                                AND userid=$value->userid 
+                            ORDER BY `timecreated` ASC
+                            LIMIT 1;";
+        $timestop = $DB->get_records_sql($get_timestopped);
+        //echo '<pre>'; print_r($timestop); echo '</pre>';
+
+        foreach($timestop as $value){
+            $array[$c][timestop]=$value->timecreated;
+        }
+        if (!isset($array[$c][timestop])){
+            $array[$c][timestop]=$array[$c][timestart];
+        }
+        $array[$c][time_diff]=($array[$c][timestop]-$array[$c][timestart])/(60*60);
+        if ($array[$c][time_diff]>=4){
+            $array[$c][time_diff]=4;
+        }
+        $c++;
+    }
+    // echo "c".$c;
+    // echo '<pre>'; print_r($array); echo '</pre>';
+
+    function makeDateArray($date1, $date2, $format = 'd-m-Y' ) {
+        $array_dates=array();
+        $current = strtotime($date1);
+        $date2 = strtotime($date2);
+        $stepVal = '+1 day';
+        while( $current <= $date2 ) {
+            $array_dates[date($format, $current)] = 0;
+            $current = strtotime($stepVal, $current);
+        }
+        return $array_dates;
+    }
+    $date_get=makeDateArray($oneMonthAgo, $today);
+    //echo '<pre>'; print_r($date_get); echo '</pre>';
+
+    $c2=0;
+    $c3=0;
+    $array2=array();
+    foreach($joined as $sco_value){
+        if ($sco_value->sco_id%2==0){
+            echo $sco_value->sco_name.'<br>';
+            for($c2 = 0; $c2 <= $c; $c2++){
+                if ($array[$c2][sco_name]==$sco_value->sco_name){
+                    // echo $oneMonthAgo.'<br>';
+                    // echo $today.'<br>';
+                    //if (strtotime($array[$c2][date])<=strtotime($today)){
+                    if (strtotime($array[$c2][date])>=strtotime($oneMonthAgo)){
+                        // echo '<br>'.strtotime($oneMonthAgo);
+                        // echo '<br>'.$array[$c2][date].'<br>';
+                    // }
+                        //$date_get[$array[$c2][date]]++;
+                        $date_get[$array[$c2][date]]+=$array[$c2][time_diff];
+                    }
+                }
+            }
+            echo '<pre>'; print_r($date_get); echo '</pre>';
+
+            //echo '<pre>'; print_r($date_get); echo '</pre>';
+            $time=array(2, 4, 10, 3, 2, 3, 4, 3, 4, 3, 4, 5, 9, 5, 6, 2, 23, 4, 3, 23, 1, 4, 3, 5, 3, 5, 6, 11, 2, 3, 21, 2, 4, 10, 3, 2, 3, 4, 3, 4, 3, 4, 5, 9, 5, 6, 2, 23, 4, 3, 23, 1, 4, 3, 5, 3, 5, 6, 11, 2, 3, 21);
+            $time_per_student = new core\chart_series($sco_value->sco_name, $time);
+            // $time_per_student = new core\chart_series($sco_value->sco_name, $date_get);
+            $chart->add_series($time_per_student);
+            $date_get=makeDateArray($oneMonthAgo, $today);
+        }
+    }
+
+
+    // $date=displayDates("16-6-2020", "27-6-2020");
+    $chart->set_labels($date_find);
+
+}
+    //renders chart if index valid
+    if ($doIExist==1 && $index!=NULL){
+        echo $OUTPUT->render($chart);
+    }
+}   
+   echo '</div>';
 
    echo $OUTPUT->container_end();
 
