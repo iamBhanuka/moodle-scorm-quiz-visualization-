22de00c5e040de68a06f042c13f7534ce26261a5
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-27 22:29:34 +0530

    Complete

--- tracker5/version.php
+++ tracker5/version.php
@@ -4,4 +4,4 @@ $plugin->version = 2018072501;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 3.0).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v4.1.1';
-$plugin->component = 'block_analytics_graphs';
+$plugin->component = 'block_tracker5';
