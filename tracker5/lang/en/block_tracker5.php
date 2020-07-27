cf432c9e37406337ad30bb66efd3ff26d23e3de1
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-27 22:28:17 +0530

    commit

--- tracker5/lang/en/block_tracker5.php
+++ tracker5/lang/en/block_tracker5.php
@@ -4,5 +4,5 @@ $string['tracker5:addinstance'] = 'Add a new simple tracker5 block';
 $string['tracker5:myaddinstance'] = 'Add a new simple tracker5 block to the My Moodle page';
 $string['pluginname'] = 'Simple tracker5 block';
 $string['tracker5'] = 'Simple tracker5';
-$string['access_graph'] = 'Access tracker5';
+$string['timeaccesseschart'] = 'Access tracker5';
 
