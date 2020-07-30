0a48d5ec4507ca5e678b1d15efbd31a3d3255323
SanduniAbenayaka <sanduni.madu1996@gmail.com>
2020-07-31 02:50:41 +0530

    add block_assignmentmarks.php file

---
+++ assignmentmarks/lang/en/block_assignmentmarks.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname']='Assignments Marks';
+$string['assignmentmarks']='My Assignmentmarks Block';
+$string['overview']='overview of students result';
+$string['assignmentmarks:addinstance']='Add a new Assignmentmarks block';
+$string['assignmentmarks:myaddinstance']='Add a new Assignmentmarksh block to the my moodle page';
