2f55c867b313627d53037c53b0dfa9f71dab4dcd
Bhanuka <mail@bhanuka.me>
2020-10-04 17:45:45 +0530

    delete

--- Moodle_plugin_Scorm_Quiz/lang/en/block_myscorm.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string['pluginname'] = ' myscorm 1 Block';
-$string['myscorm'] = 'myscorm 1';
-$string['blockstring'] = 'BlockString';
-$string['myscorm:addinstance'] = 'Add a new simple HTML block';
-$string['myscorm:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
--- Scorm_Summary/lang/en/block_scormsum.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string['pluginname'] = 'Simple scormsum Block';
-$string['scormsum'] = 'Simple scormsum';
-$string['blockstring'] = 'BlockString';
-$string['scormsum:addinstance'] = 'Add a new simple HTML block';
-$string['scormsum:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
