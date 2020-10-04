a440a473004c7d20443388fd3cd74c73cb9824b3
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-10-04 17:51:39 +0530

    deleted

--- tracker5/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2018072501;  // YYYYMMDDHH (year, month, day, 24-hr time).
-$plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 3.0).
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v4.1.1';
-$plugin->component = 'block_tracker5';
